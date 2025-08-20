42a854a53bd4e1cdd7ecf097756f99d84af3f51a
brianna-mitri <brianna.mitri@gmail.com>
2025-08-20 08:24:37 -0700

    Updated navbar menu to close when clicked outside (mobile) and toggle transition slight update

--- docs/assets/js/navbar.js
+++ docs/assets/js/navbar.js
@@ -1,34 +1,37 @@
-// const navEl = document.querySelector('.navbar');
+/* -------- Transition navbar from transparent color theme to target theme (navbar-theme-info) -------- */
+// transparent navbar element
+const transparentNavEl = document.querySelector('.navbar-theme-transparent');
 
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY > 50) {
-//         // Set the sliding panel color to match info theme
-//         navEl.style.setProperty('--navbar-slide-bg', getComputedStyle(document.documentElement).getPropertyValue('--bs-light'));
-//         navEl.classList.add('navbar-scrolled');
-//         navEl.classList.remove('navbar-theme-transparent');
-//         navEl.classList.add('navbar-theme-info');
-//     } else {
-//         navEl.classList.remove('navbar-scrolled');
-//         navEl.classList.remove('navbar-theme-info');
-//         navEl.classList.add('navbar-theme-transparent');
-//     }
-// });
-
-const navEl = document.querySelector('.navbar');
-
-
+// scroll listener
+if (transparentNavEl)
 window.addEventListener('scroll', () => {
-    const currentScrollY = window.scrollY;
-    if (currentScrollY > 50) {
-        navEl.classList.add('navbar-theme-info');
-    } else {
-        navEl.classList.remove('navbar-theme-info');
-    }
+    // const currentScrollY = window.scrollY;
 
-   
-    // if (window.scrollY < 56) {
-    //     navEl.classList.remove('navbar-theme-info');
-    // } else {
-    //     navEl.classList.add('navbar-theme-info');
-    // }
+    // scrolling down add target theme--remove if scrolling up
+    if (window.scrollY > 50) {
+        transparentNavEl.classList.add('navbar-theme-info');
+    } else {
+        transparentNavEl.classList.remove('navbar-theme-info');
+    }
+});
+
+/* -------- Close menu dropdown if clicking outside of menu -------- */
+// wait for page to fully load
+document.addEventListener('DOMContentLoaded', function() {
+
+    // click listener to entire document
+    document.addEventListener('click', function(event) {
+
+        // navbar menu and toggler elements
+        const menuEl = document.querySelector('#navbarMenu');
+        const togglerEl = document.querySelector('.navbar-toggler');
+
+        // if 1. menu exists, 2. menu is currently, and 3. click wasn't inside menu or toggler button
+        // then close menu by clicking on toggler
+        if (menuEl && menuEl.classList.contains('show') &&
+            !menuEl.contains(event.target) &&
+            !togglerEl.contains(event.target)) {
+                togglerEl.click();
+            }
+    });
 });
